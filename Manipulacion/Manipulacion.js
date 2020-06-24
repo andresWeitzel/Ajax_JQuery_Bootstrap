f63d171abb9b8fb2d3f8cd946a6cce7af6e71df4
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-06-24 13:39:22 -0300

    Add files via upload

--- Manipulacion/Manipulacion.js
+++ Manipulacion/Manipulacion.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
-    $("div .card-body").click(function() {
-        $("div .card-body").css("color", "#DF01A5");
+
+
+    var incluirParrafo = $("<p id='parrafoAgregado'>Parrafo Agregado</p>");
+    var incluirLista = $("<ul id='listaAgregada'><li>Lista1</li><li>Lista2</li></ul>");
+
+    $("button#agregar").click(function() {
+        $("body").append(incluirParrafo);
+        $("body").append(incluirLista);
     });
+
+    $("button#modificar").click(function() {
+        $("p#parrafoAgregado,ul#listaAgregada > li")
+            .css({
+                color: "black",
+                background: "#ccf0f9"
+            });
+
+
+    });
+
+    $("button#remover").click(function() {
+        $("p").remove('#parrafoAgregado');
+        $("ul").remove('#listaAgregada');
+    });
+
+
+
 });
